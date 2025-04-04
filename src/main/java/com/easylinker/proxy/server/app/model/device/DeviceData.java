f81d56450746250c8f720b429450ac8dcf84f79d
xi683235 <ibxgwgwsvpy11y@126.com>
2025-04-04 23:29:39 +0800

    data字段改为text

--- src/main/java/com/easylinker/proxy/server/app/model/device/DeviceData.java
+++ src/main/java/com/easylinker/proxy/server/app/model/device/DeviceData.java
@@ -2,15 +2,15 @@ package com.easylinker.proxy.server.app.model.device;
 
 import com.easylinker.proxy.server.app.model.base.BaseEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 /**
  * 设备数据
  */
 @Entity
 public class DeviceData extends BaseEntity {
+    @Lob
+    @Column(columnDefinition = "TEXT")
     private String data;
     @ManyToOne(targetEntity = Device.class, fetch = FetchType.LAZY)
     private Device device;
