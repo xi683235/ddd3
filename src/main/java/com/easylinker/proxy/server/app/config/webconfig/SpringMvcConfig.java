7a8de86336487a907b191cc8efac13482810b872
xi683235 <ibxgwgwsvpy11y@126.com>
2025-06-13 15:50:39 +0800

    增加访问日志拦截器

--- src/main/java/com/easylinker/proxy/server/app/config/webconfig/SpringMvcConfig.java
+++ src/main/java/com/easylinker/proxy/server/app/config/webconfig/SpringMvcConfig.java
@@ -1,12 +1,19 @@
 package com.easylinker.proxy.server.app.config.webconfig;
 
+import com.easylinker.proxy.server.app.interceptor.AccessLogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by wwhai on 2018/3/14.
  */
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AccessLogInterceptor());
+    }
+
+
 }
---
+++ src/main/java/com/easylinker/proxy/server/app/interceptor/AccessLogInterceptor.java
@@ -0,0 +1,44 @@
+package com.easylinker.proxy.server.app.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 请求日志拦截器
+ */
+@Component
+public class AccessLogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println(getIp(request));
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+    //获取客户端IP
+    private String getIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
