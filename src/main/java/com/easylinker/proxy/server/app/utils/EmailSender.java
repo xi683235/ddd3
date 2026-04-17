976eb92696b99d3fa0447fe5534394fe21f688a7
xi683235 <ibxgwgwsvpy11y@126.com>
2026-04-17 20:49:10 +0800

    增加了邮件发送模板

--- src/main/java/com/easylinker/proxy/server/app/utils/EmailSender.java
+++ src/main/java/com/easylinker/proxy/server/app/utils/EmailSender.java
@@ -109,44 +109,4 @@ public class EmailSender {
 
     }
 
-    private class SMSTemplate {
-        String title;
-        String group;
-        String url;
-
-        public SMSTemplate() {
-        }
-
-        public SMSTemplate(String title, String group, String url) {
-
-            this.title = title;
-            this.group = group;
-            this.url = url;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getGroup() {
-            return group;
-        }
-
-        public void setGroup(String group) {
-            this.group = group;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-    }
-
 }
